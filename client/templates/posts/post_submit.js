05c458832741620a695b11cc81634f03c15a0e4a
Alejandro Reyna <aleejandroreyna@gmail.com>
2016-01-22 15:12:52 -0600

    add validation in post submit

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,11 +1,30 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({ //se especifican los eventos del Template postSubmit
 
   'submit form': function(e) { //Se especifica el evento submit del form
     e.preventDefault(); //Se retira el action por defecto
+
     var post = { // se declara un objeto que contendrá los valores a enviar
       url: $(e.target).find('[name=url]').val(), //parametro url
       title: $(e.target).find('[name=title]').val() //parámetro título
     };
+
+    var errors = validatePost(post);
+    if (errors.title || errors.url) {
+      return Session.set('postSubmitErrors', errors);
+    }
 
     Meteor.call('postInsert', post, function(error, result) {
     //se intenta llamar al método postInsert
