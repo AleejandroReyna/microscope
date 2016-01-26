cad96d2f39ddbc5baa5d48f89ba7a2bf758173cd
Alejandro Reyna <aleejandroreyna@gmail.com>
2016-01-26 11:28:23 -0600

    simple publication/suscription for comments

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,9 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function() { //suscribción post
+  return Posts.find(); //retorna todos los post
 });
 
 
-Meteor.publish('comments', function() {
-  return Comments.find();
+Meteor.publish('comments', function(postId) { //suscripción comments recibe id del post
+  check(postId, String); //verifica que postId sea un string
+  return Comments.find({postId: postId}); //retorna comments solo con PostId
 });
