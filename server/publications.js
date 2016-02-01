471c64cea8c9f98725baf3a505195909b97919bd
Alejandro Reyna <aleejandroreyna@gmail.com>
2016-02-01 17:27:39 -0600

    added animations

--- server/publications.js
+++ server/publications.js
@@ -21,3 +21,11 @@ Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId, read: false});
   //por los parámetros solo devolverá las notificaciones con el userId y que no han sido leídos
 });
+
+// Meteor.publish('newPosts', function(limit) { //Creamos la publicación para los elementos de manera en que los muestre por defecto desde el más nuevo
+//   return Posts.find({}, {sort: {submitted: -1}, limit: limit}); //la función retornará Todos los post, desde su límite, en orden de creación
+// });
+
+// Meteor.publish('bestPosts', function(limit) {//Creamos la publicación para los más votados
+//   return Posts.find({}, {sort: {votes: -1, submitted: -1}, limit: limit}); //los mostrará en relación con su número de votos en orden asc, seguido por su orden de creación y limitados por el límite
+// });
